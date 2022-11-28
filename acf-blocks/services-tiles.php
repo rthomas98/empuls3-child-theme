164411de6f02701609d4c13b968b7dbc6dbc41a8
Rob Thomas <rob.thomas@howarddc.com>
2022-11-28 11:08:25 -0600

    fixed icon on the tile

--- acf-blocks/services-tiles.php
+++ acf-blocks/services-tiles.php
@@ -21,7 +21,9 @@
                 <div class="row mb-5">
 				<?php while ( have_rows( 'tiles' ) ) : the_row(); ?>
 					<a href="<?php the_sub_field( 'link' ); ?>" class="tile p-5 col-sm-12 col-md-6 col-lg-4 text-center">
+                   <div class="icon-bg">
                     <i class="<?php the_sub_field( 'icon' ); ?>"></i>
+                   </div>
 					<h3><?php the_sub_field( 'title' ); ?></h3>
 					<p>
                         <?php the_sub_field( 'content' ); ?>
